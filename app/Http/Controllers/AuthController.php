8ce340cdfadde28c1f234929828af0bbd6de0e55
Trata-simvolav <114504094+Trata-simvolav@users.noreply.github.com>
2024-03-24 15:41:02 +0500

    0.1.1

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -10,11 +10,11 @@ class AuthController extends Controller
 {
     public function addUser(Request $request)
     {
-        $path = $request->input('pathToFile');
-        $nickname = $request->input('nickname');
-        $filename = $request->input('filename');
+        $name = $request->input('name');
+        $password = $request->input('password');
+        $email = $request->input('email');
         $type = $request->input('type');
 
-        return 'alarm';
+        return true;
     }
 }
